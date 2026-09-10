97d413c1fd31e9ece84861e2e47ea8fed4cd4fca
Joosung Min <joosung.min.mil@army.mil>
2026-09-10 16:12:18 -0400

    vault:pki:root-ca

--- services/vault/scripts/vault-pki.sh
+++ services/vault/scripts/vault-pki.sh
@@ -75,6 +75,55 @@ create_pki_mount() {
     log "PKI engine mounted at pki/."
 }
 
+configure_pki_root() {
+    log "Configuring root PKI..."
+
+    local response
+
+    if ! response="$(curl -fsS --max-time 10 \
+        --cacert "${CA_CRT}" \
+        --resolve "${VAULT_HOST}:${VAULT_PORT}:${CLOUDBR0_IP}" \
+        --header "X-Vault-Token: ${TOKEN}" \
+        "${VAULT_API}/v1/pki/ca/pem")"; then
+        die "Could not read the root CA from pki/."
+    fi
+
+    case "$response" in
+        "")
+            log "No root yet. Generating it..."
+            create_pki_root
+            ;;
+        *"BEGIN CERTIFICATE"*)
+            log "Root CA already exists at pki/."
+            ;;
+        *)
+            die "Unexpected response from pki/ca/pem. Refusing to generate a root."
+            ;;
+    esac
+}
+
+create_pki_root() {
+    log "Generating root CA..."
+
+    curl -fsS --max-time 10 \
+        --cacert "${CA_CRT}" \
+        --resolve "${VAULT_HOST}:${VAULT_PORT}:${CLOUDBR0_IP}" \
+        --header "X-Vault-Token: ${TOKEN}" \
+        --header "Content-Type: application/json" \
+        --request POST \
+        --data '{
+            "common_name": "SingleClusterIaCLab Root CA",
+            "ttl": "87600h",
+            "key_type": "rsa",
+            "key_bits": 4096,
+            "issuer_name": "lab-root"
+        }' \
+        "${VAULT_API}/v1/pki/root/generate/internal" ||
+        die "Could not create the root CA."
+
+    log "Root CA created at /pki"
+}
+
 main () {
     TOKEN="$(jq -r '.root_token' "${INIT_FILE}")"
     [[ -n "${TOKEN}" && "${TOKEN}" != null ]] || \
@@ -83,6 +132,7 @@ main () {
     CLOUDBR0_IP="$(get_cloudbr0_ip)"
 
     configure_pki_mount
+    configure_pki_root
 }
 
 main "$@"
